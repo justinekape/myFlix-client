7a47aa0b2c83e6b837c3e474ba938b3811a59d43
justine <106638324+justinekape@users.noreply.github.com>
2022-10-31 13:33:27 -0400

    integrate axios
    temp remove auth for '/movies'

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,28 +8,21 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { 
-          _id: 1, 
-          Title: 'Inception', 
-          Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', 
-          ImagePath: 'inception.jpg'
-        },
-        { 
-          _id: 2, 
-          Title: 'The Shawshank Redemption', 
-          Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 
-          ImagePath: 'shawshank.jpg'
-        },
-        {
-          _id: 3, 
-          Title: 'Gladiator', 
-          Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 
-          ImagePath: 'gladiator.png'
-        }
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('https://jb-myflix.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -42,14 +36,14 @@ export class MainView extends React.Component {
 
    //if (selectedMovie) return <MovieView movie={selectedMovie} />;
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>; 
+    if (movies.length === 0) return <div className="main-view" />; 
     
     return (
         <div className="main-view">
           {selectedMovie
             ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
             : movies.map(movie => (
-              <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie)}}/>
+              <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
             ))
           }
         </div>
